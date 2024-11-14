10ce4b20930b0a076046822d2143973fb96652ea
Johan Abelson <johan@rabadang.se>
2024-11-14 13:12:07 +0100

    Added template for form page

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,21 @@
 const express = require("express");
 const app = express();
 var xml = require("xml");
+const path = require("path");
+
+import { Eta } from "eta";
+
+
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get("/", async (req: any, res: any) => {
   // Data: Simulerar data från radbaserad fil
-
+  const eta = new Eta({ views: path.join(__dirname, "templates") })
   const { rowbasedDataString } = require("../modules/data");
   rowbasedDataString().then((data: string) => {
     console.log("data", data);
-    res.send(`<!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>Document</title>
-                </head>
-                <body style="padding: 0; font-family: verdana;">
-                    <h1>Radbasera data</h1>
-                    <textarea readonly style="margin: 0;position: absolute; top: 100px; right: 10px; bottom: 100px; left: 10px; padding: 10px;">${data}</textarea>
-                    <div style="display: flex; align-items: center; justify-content: center; position: absolute; width: 100vw; bottom: 0; height: 100px;">
-                        <a href="/xml" style="background-color: lightblue; color: #000; padding: 10px 16px; border-radius: 3px; text-decoration: none;">Konvertera 🐣</a>
-                    </div>
-                </body>
-                </html>`);
+    const page = eta.render("./form.eta", { data });
+    res.send(page);
     return data;
   });
 });
